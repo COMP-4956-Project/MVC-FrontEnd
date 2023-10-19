6cc2ec836a1735b43ef070f9193c11a2832f36e3
TrevorHontg <trevorhong159@gmail.com>
2023-10-19 12:41:51 -0700

    Sidebar

--- wwwroot/js/testing.js
+++ wwwroot/js/testing.js
@@ -1,42 +1,52 @@
-import { AssignmentBlock, EqualityBlock, FunctionBlock } from "./classes/CodeBlock.js";
+import { AssignmentBlock, EqualityBlock, ExpressionBlock, FunctionBlock, LogicBlock, ScopeBlock } from "./classes/CodeBlock.js";
+import { lineMaker } from "./drag_drop.js";
+import { VariableBlock, LiteralBlock, DummyLiteralBlock } from "./classes/ValueBlock.js";
 
 function test(){
     alert("Testing");
 }
 
-function allowDrop(ev) 
-{
-    ev.preventDefault();
-}
-  
-function drop(ev) 
-{
-  ev.preventDefault();
-
-  if(ev.target.className != "testContainer")
-  {
-    return;
-  }
-
-  let data = ev.dataTransfer.getData("key");
-  let block = document.getElementById(data);
-  ev.target.appendChild(block);
-}
-
-function drag(ev) 
-{
-  ev.dataTransfer.setData("key", ev.target.id);
-}
-
-window.allowDrop = allowDrop;
-window.drop = drop;
-window.drag = drag;
-
 let test1 = document.getElementById("test1");
-let block = new EqualityBlock("==", 1, 1); 
-//block.element.addEventListener("dragstart", function(event){drag(event)});
-let block2 = new AssignmentBlock("=", 1, 1);
-let block3 = new FunctionBlock("function", "test", 1, 1);
+
+let block = new EqualityBlock("==");
+block.element.className += " dummy"
+
+let block2 = new AssignmentBlock("+=");
+block2.element.className += " dummy"
+
+let block3 = new FunctionBlock("function");
+block3.element.className += " dummy"
+
+let block4 = new ExpressionBlock("+");
+block4.element.className += " dummy"
+//block4.makeRightSide();
+
+let block5 = new LogicBlock("and");
+block5.element.className += " dummy"
+
+let block6 = new ScopeBlock("if");
+block6.element.className += " dummy"
+
+let block7 = new ScopeBlock("else");
+block7.element.className += " dummy"
+
+
+
 test1.appendChild(block.element);
 test1.appendChild(block2.element);
 test1.appendChild(block3.element);
+test1.appendChild(block4.element);
+test1.appendChild(block5.element);
+test1.appendChild(block6.element);
+test1.appendChild(block7.element);
+
+
+let variableBlock = new VariableBlock("x", "number", 6);
+let literalBlock = new DummyLiteralBlock("number");
+
+//test1.appendChild(variableBlock.element);
+test1.appendChild(literalBlock.element);
+
+lineMaker(document.getElementById("test2"));
+
+
