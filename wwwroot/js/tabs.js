60568dd261d1498471c571622e10bc5ddf133c60
traillio <tlyall2@my.bcit.ca>
2023-10-22 12:17:10 -0700

    add lines to new tabs

--- wwwroot/js/tabs.js
+++ wwwroot/js/tabs.js
@@ -1,3 +1,5 @@
+import { lineMaker } from "./drag_drop.js";
+
 $(document).ready(function() {
     let tabCounter = 1;
 
@@ -18,7 +20,10 @@ $(document).ready(function() {
     // Adding new tabs
     $('#addTab').click(function() {
         tabCounter++;
+        let tabId = "tab-" + tabCounter;
         $('.tab-headers').append(`<div class="tab new-tab-style" data-tab-id="${tabCounter}"><span class="tab-title">Tab ${tabCounter}</span><button class="close-tab">x</button></div>`);
-        $('.tab-contents').append(`<div class="tab-content" data-tab-id="${tabCounter}">Content for Tab ${tabCounter}</div>`);
+        $('.tab-contents').append(`<div class="tab-content line-container" id="${tabId}" data-tab-id="${tabCounter}">Content for Tab ${tabCounter}</div>`);
+
+        lineMaker(document.getElementById(tabId));
     });
 });
