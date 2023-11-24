0efbf92c4ec09dc4236a3f7a2e80f49ddceb192f
shayanz <shayan.zahedana@gmail.com>
2023-11-23 16:32:58 -0800

    merged main into this

--- MVC-Frontend-Backend/Program.cs
+++ MVC-Frontend-Backend/Program.cs
@@ -1,22 +1,42 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MVC_Backend_Frontend;
+using MVC_Backend_Frontend.Data;
 using MVC_Backend_Frontend.Models;
 using System.Text.Json;
 using System.Text.Json.Nodes;
-using System.Text.RegularExpressions;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
-string json =
-    "{\"blocks\":[{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_variable\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":0}},{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"Hello World!\"}},{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"operation\",\"operation\":\"add\",\"A\":{\"type\":\"value\",\"field\":\"num\",\"num\":1},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":2}}},{\"type\":\"control\",\"instruction\":\"while\",\"input\":{\"type\":\"logic\",\"logic\":\"less\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":5}},\"children\":[{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"hello\"}},{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_add\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":1}}]}]}";
-BlockList blockList = Newtonsoft.Json.JsonConvert.DeserializeObject<BlockList>(json);
-string codeResult = Regex.Escape(BlockListParser.ParseBlockList(blockList));
-Console.WriteLine(BlockListParser.ParseBlockList(blockList));
-Console.WriteLine(codeResult);
+string json = File.ReadAllText(@"./test3.json");
+var pythonRunner = new PythonRunner();
+pythonRunner.RunFromBlockList(JsonSerializer.Deserialize<BlockList>(json)!);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+/*builder.Services.AddDefaultIdentity<CustomUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();*/
+
+builder.Services.AddIdentity<CustomUser, CustomRole>(
+options => {
+    options.Stores.MaxLengthForKeys = 128;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultUI()
+.AddDefaultTokenProviders()
+.AddRoles<CustomRole>();
+
+var mongoString = "mongodb+srv://admin:Wk2hn25gmfv1JpFh@comp4956.9hedzlx.mongodb.net/?retryWrites=true&w=majority"; // Update with your MongoDB server connection string
+var client = new MongoClient(mongoString);  // Replace with your database name
+
+// Add MongoDB services
+builder.Services.AddSingleton<IMongoClient>(client);
 
 builder.Services.AddControllersWithViews();
 
@@ -41,8 +61,31 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
+// Routing to the Help page
+app.MapControllerRoute(
+    name: "help",
+    pattern: "{controller=Help}/{action=Help}/{id?}");
+
+// Routing to the Privacy page
+app.MapControllerRoute(
+    name: "privacy",
+    pattern: "{controller=Privacy}/{action=Privacy}/{id?}");
+
+// Routing to the Login page
+app.MapControllerRoute(
+    name: "login",
+    pattern: "{controller=Login}/{action=Login}/{id?}");
+
+// Routing to the Logout page
+app.MapControllerRoute(
+    name: "logout",
+    pattern: "{controller=Logout}/{action=Logout}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
