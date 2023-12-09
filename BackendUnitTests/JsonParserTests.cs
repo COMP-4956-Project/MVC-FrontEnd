65b1bb4b038dd1b2e5667c4f3c65e5480697c269
shayanz <shayan.zahedana@gmail.com>
2023-12-08 23:55:22 -0800

    working on merge

--- BackendUnitTests/JsonParserTests.cs
+++ BackendUnitTests/JsonParserTests.cs
@@ -17,7 +17,7 @@ public class JsonParserTests
             "{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"Hello World!\"}}";
         string expectedCode = "print('Hello World!')";
         Block block = Newtonsoft.Json.JsonConvert.DeserializeObject<Block>(json);
-        string returnedCode = BlockListParser.ParseBlock(block);
+        string returnedCode = JsonBlockParser.ParseBlock(block);
         Assert.That(returnedCode, Is.EqualTo(expectedCode));
     }
     
@@ -28,7 +28,7 @@ public class JsonParserTests
             "{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_variable\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":0}}";
         string expectedCode = "index_i = 0";
         Block block = Newtonsoft.Json.JsonConvert.DeserializeObject<Block>(json);
-        string returnedCode = BlockListParser.ParseBlock(block);
+        string returnedCode = JsonBlockParser.ParseBlock(block);
         Assert.That(returnedCode, Is.EqualTo(expectedCode));
     }
     
@@ -39,7 +39,7 @@ public class JsonParserTests
             "{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"operation\",\"operation\":\"add\",\"A\":{\"type\":\"value\",\"field\":\"num\",\"num\":1},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":2}}}";
         string expectedCode = "print(1 + 2)";
         Block block = Newtonsoft.Json.JsonConvert.DeserializeObject<Block>(json);
-        string returnedCode = BlockListParser.ParseBlock(block);
+        string returnedCode = JsonBlockParser.ParseBlock(block);
         Assert.That(returnedCode, Is.EqualTo(expectedCode));
     }
     
@@ -60,7 +60,9 @@ public class JsonParserTests
         string expectedCode = "index_i\\ =\\ 0\\nwhile\\ index_i\\ <\\ 5:\\n\\tprint\\('hello\\ world'\\)\\n\\tindex_i\\ \\+=\\ 1\\n\\tindex_j\\ =\\ 0\\n\\twhile\\ index_j\\ <\\ 5:\\n\\t" +
                               "\\tprint\\(index_i\\ \\+\\ index_j\\)\\n\\t\\tindex_j\\ \\+=\\ 1\\nprint\\('hello\\ world'\\)\\n";
         BlockList blockList = Newtonsoft.Json.JsonConvert.DeserializeObject<BlockList>(json);
-        string codeResult = Regex.Escape(BlockListParser.ParseBlockList(blockList));
+        string codeResult = Regex.Escape(JsonBlockParser.ParseBlockList(blockList));
+        Console.WriteLine(expectedCode);
+        Console.WriteLine(codeResult);
         Assert.That(codeResult, Is.EqualTo(expectedCode));
     }
     
@@ -71,8 +73,8 @@ public class JsonParserTests
             "{\"blocks\":[{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_variable\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":0}},{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"Hello World!\"}},{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"operation\",\"operation\":\"add\",\"A\":{\"type\":\"value\",\"field\":\"num\",\"num\":1},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":2}}},{\"type\":\"control\",\"instruction\":\"while\",\"input\":{\"type\":\"logic\",\"logic\":\"less\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":5}},\"children\":[{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"hello\"}},{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_add\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":1}}]}]}";
         string expectedCode = "index_i\\ =\\ 0\\nprint\\('Hello\\ World!'\\)\\nprint\\(1\\ \\+\\ 2\\)\\nwhile\\ index_i\\ <\\ 5:\\n\\tprint\\('hello'\\)\\n\\tindex_i\\ \\+=\\ 1\\n";
         BlockList blockList = Newtonsoft.Json.JsonConvert.DeserializeObject<BlockList>(json);
-        string codeResult = Regex.Escape(BlockListParser.ParseBlockList(blockList));
-        Console.WriteLine(BlockListParser.ParseBlockList(blockList));
+        string codeResult = Regex.Escape(JsonBlockParser.ParseBlockList(blockList));
+        Console.WriteLine(JsonBlockParser.ParseBlockList(blockList));
         Assert.That(codeResult, Is.EqualTo(expectedCode));
     }
 }
--- BackendUnitTests/PythonControllerTests.cs
+++ BackendUnitTests/PythonControllerTests.cs
@@ -9,14 +9,14 @@ namespace BackendUnitTestProject;
 public class PythonControllerTests
 {
     [Test]
-    public void PythonControlllerTest1()
+    public async Task PythonControlllerTest1Async()
     {
         string json =
             "{\"blocks\":[{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_variable\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":0}},{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"Hello World!\"}},{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"operation\",\"operation\":\"add\",\"A\":{\"type\":\"value\",\"field\":\"num\",\"num\":1},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":2}}},{\"type\":\"control\",\"instruction\":\"while\",\"input\":{\"type\":\"logic\",\"logic\":\"less\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":5}},\"children\":[{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"hello\"}},{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_add\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":1}}]}]}";
         BlockList blockList = Newtonsoft.Json.JsonConvert.DeserializeObject<BlockList>(json);
         string expectedResult = "Hello World!\n3\nhello\nhello\nhello\nhello\nhello\n";
         PythonController pythonController = new PythonController();
-        IActionResult actionResult = pythonController.PostPythonFromJson(blockList);
+        IActionResult actionResult = await pythonController.PostPythonFromJson(blockList);
         var okResult = actionResult as OkObjectResult;
         string result = Regex.Unescape(okResult.Value as string);
         result = result.Replace("\r", "");
