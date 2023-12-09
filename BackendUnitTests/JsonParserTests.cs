60cb4ea5e5f940cd6d11d1167dbb11d8834f17e2
shayanz <shayan.zahedana@gmail.com>
2023-12-09 00:15:17 -0800

    fixed unit tests

--- BackendUnitTests/JsonParserTests.cs
+++ BackendUnitTests/JsonParserTests.cs
@@ -57,12 +57,10 @@ public class JsonParserTests
             "\"input\":{\"type\":\"value\",\"field\":\"operation\",\"operation\":\"add\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"variable\",\"name\":\"index_j\"}}}," +
             "{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_add\",\"A\":{\"type\":\"variable\",\"name\":\"index_j\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":1}}]}]}," +
             "{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"hello world\"}}]}";
-        string expectedCode = "index_i\\ =\\ 0\\nwhile\\ index_i\\ <\\ 5:\\n\\tprint\\('hello\\ world'\\)\\n\\tindex_i\\ \\+=\\ 1\\n\\tindex_j\\ =\\ 0\\n\\twhile\\ index_j\\ <\\ 5:\\n\\t" +
-                              "\\tprint\\(index_i\\ \\+\\ index_j\\)\\n\\t\\tindex_j\\ \\+=\\ 1\\nprint\\('hello\\ world'\\)\\n";
+        string expectedCode = "index_i\\ =\\ 0\\nwhile\\ index_i\\ <\\ 5:\\n\\ \\ \\ \\ print\\('hello\\ world'\\)\\n\\ \\ \\ \\ index_i\\ \\+=\\ 1\\n\\ \\ \\ \\ index_j\\ =\\ 0\\n\\ \\ \\ \\ while\\ index_j\\ <\\ 5:\\n\\ \\ \\ \\ " +
+                              "\\ \\ \\ \\ print\\(index_i\\ \\+\\ index_j\\)\\n\\ \\ \\ \\ \\ \\ \\ \\ index_j\\ \\+=\\ 1\\nprint\\('hello\\ world'\\)\\n";
         BlockList blockList = Newtonsoft.Json.JsonConvert.DeserializeObject<BlockList>(json);
         string codeResult = Regex.Escape(JsonBlockParser.ParseBlockList(blockList));
-        Console.WriteLine(expectedCode);
-        Console.WriteLine(codeResult);
         Assert.That(codeResult, Is.EqualTo(expectedCode));
     }
     
@@ -71,10 +69,9 @@ public class JsonParserTests
     {
         string json =
             "{\"blocks\":[{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_variable\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":0}},{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"Hello World!\"}},{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"operation\",\"operation\":\"add\",\"A\":{\"type\":\"value\",\"field\":\"num\",\"num\":1},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":2}}},{\"type\":\"control\",\"instruction\":\"while\",\"input\":{\"type\":\"logic\",\"logic\":\"less\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":5}},\"children\":[{\"type\":\"function\",\"instruction\":\"print\",\"input\":{\"type\":\"value\",\"field\":\"text\",\"text\":\"hello\"}},{\"type\":\"function\",\"field\":\"operation\",\"operation\":\"assign_add\",\"A\":{\"type\":\"variable\",\"name\":\"index_i\"},\"B\":{\"type\":\"value\",\"field\":\"num\",\"num\":1}}]}]}";
-        string expectedCode = "index_i\\ =\\ 0\\nprint\\('Hello\\ World!'\\)\\nprint\\(1\\ \\+\\ 2\\)\\nwhile\\ index_i\\ <\\ 5:\\n\\tprint\\('hello'\\)\\n\\tindex_i\\ \\+=\\ 1\\n";
+        string expectedCode = "index_i\\ =\\ 0\\nprint\\('Hello\\ World!'\\)\\nprint\\(1\\ \\+\\ 2\\)\\nwhile\\ index_i\\ <\\ 5:\\n\\ \\ \\ \\ print\\('hello'\\)\\n\\ \\ \\ \\ index_i\\ \\+=\\ 1\\n";
         BlockList blockList = Newtonsoft.Json.JsonConvert.DeserializeObject<BlockList>(json);
         string codeResult = Regex.Escape(JsonBlockParser.ParseBlockList(blockList));
-        Console.WriteLine(JsonBlockParser.ParseBlockList(blockList));
         Assert.That(codeResult, Is.EqualTo(expectedCode));
     }
 }
